44ccfe78bfce236a6ad5c3651116d9bfcdd845d5
Projojoboy <22048@ma-web.nl>
2018-09-24 14:21:02 +0200

    Added Game

---
+++ Become Fat Shader/Assets/IncreaseFat.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseFat : MonoBehaviour {
+
+    private Renderer rend;
+    float fatness = 0;
+    [SerializeField] GameObject body;
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.tag == "food")
+        {
+            fatness += 10;
+            body.GetComponent<Renderer>().material.SetFloat("_Amount", fatness);
+            Destroy(coll.gameObject);
+        }
+
+        if (coll.tag == "SpecialFood")
+        {
+            fatness += 100;
+            transform.localScale = new Vector3(3, 3, 3);
+            body.GetComponent<Renderer>().material.SetFloat("_Amount", fatness);
+            Destroy(coll.gameObject);
+        }
+
+        if (coll.tag == "void")
+        {
+            transform.position = new Vector3(0, 10, 0);
+        }
+    }
+}
---
+++ Become Fat Shader/Assets/LookAtPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 lookAtPosition = player.transform.position;
+        lookAtPosition.y = transform.position.y;
+        transform.LookAt(lookAtPosition);
+    }
+}
